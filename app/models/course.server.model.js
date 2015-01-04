f863bc7fbc6565f408f1b1d9e13397c66332dfb5
kyle-rader <kyle.rader@ieee.org>
2015-01-04 08:45:22 -0800

    Initial application commit of the default MEAN.js app.

---
+++ app/models/course.server.model.js
@@ -0,0 +1,34 @@
+'use strict';
+
+/**
+ * Module dependencies.
+ */
+var mongoose = require('mongoose'),
+	Schema = mongoose.Schema;
+
+/**
+ * Course Schema
+ */
+var CourseSchema = new Schema({
+	name: {
+		type: String,
+		default: '',
+		required: 'Please fill Course name',
+		trim: true
+	},
+	quarter: {
+		type: String,
+		default: '',
+		trim: true
+	},
+	created: {
+		type: Date,
+		default: Date.now
+	},
+	user: {
+		type: Schema.ObjectId,
+		ref: 'User'
+	}
+});
+
+mongoose.model('Course', CourseSchema);
---
+++ public/modules/courses/config/courses.client.routes.js
@@ -0,0 +1,29 @@
+'use strict';
+
+//Setting up route
+angular.module('courses').config(['$stateProvider',
+	function($stateProvider) {
+		// Courses state routing
+		$stateProvider.
+		state('course-content', {
+			url: '/course-content',
+			templateUrl: 'modules/courses/views/course-content.client.view.html'
+		}).
+		state('listCourses', {
+			url: '/courses',
+			templateUrl: 'modules/courses/views/list-courses.client.view.html'
+		}).
+		state('createCourse', {
+			url: '/courses/create',
+			templateUrl: 'modules/courses/views/create-course.client.view.html'
+		}).
+		state('viewCourse', {
+			url: '/courses/:courseId',
+			templateUrl: 'modules/courses/views/view-course.client.view.html'
+		}).
+		state('editCourse', {
+			url: '/courses/:courseId/edit',
+			templateUrl: 'modules/courses/views/edit-course.client.view.html'
+		});
+	}
+]);
