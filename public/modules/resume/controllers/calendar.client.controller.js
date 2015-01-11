72dc39981439a9f38553883be73e6a5de3c1b7e4
kyle-rader <kyle.rader@ieee.org>
2015-01-10 19:57:53 -0800

    Size calendar iframe on page load (resume module).

--- public/modules/resume/controllers/calendar.client.controller.js
+++ public/modules/resume/controllers/calendar.client.controller.js
@@ -2,7 +2,7 @@
 
 angular.module('resume').controller('CalendarController', ['$scope',
 	function($scope) {
-		// Controller Logic
-		// ...
+		$scope.calendarWidth = window.innerWidth - Math.round((window.innerWidth * 0.2 ));
+		if ($scope.calendarWidth > 1180 ) $scope.calendarWidth = 1180;
 	}
 ]);
