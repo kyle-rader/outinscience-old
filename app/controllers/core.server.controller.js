48df4fcd0274e703e9973d8bcc01269bd7f9d7c1
kyle-rader <kyle.rader@ieee.org>
2015-02-17 06:47:59 -0800

    Add check to mail subscribe route to only subscribe on production.

--- app/controllers/core.server.controller.js
+++ app/controllers/core.server.controller.js
@@ -19,7 +19,7 @@ exports.index = function(req, res) {
 exports.emailSubscribe = function(req, res) {
 	var validEmail = validator.validate(req.params.email);
 
-	if (validEmail) {
+	if (process.env.NODE_ENV == 'production' && validEmail) {
 		mc.lists.subscribe({id: req.params.listId, email:{email:req.params.email}},
 		function(data) {
 			res.send({email: req.params.eamil});
@@ -27,7 +27,9 @@ exports.emailSubscribe = function(req, res) {
 		function(error) {
 			res.send({email: null, error: 'There was an error subscribing that email.\n' + error.error});
 		});
+	} else if (process.env.NODE_ENV == 'development') {
+		res.send({email: null, error: 'This email "' + req.params.email + '" would have been subscribe - but you are hitting a dev server.'});
 	} else {
-		res.send({email: null, error: 'Invalid email address format'});
+		res.send({email: req.params.email + ' - Running on dev - no email sent.', error: 'Invalid email address format'});
 	}
 };
