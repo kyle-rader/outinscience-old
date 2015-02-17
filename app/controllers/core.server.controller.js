84fe257e5d4224dcf0196afdba40b68f9e6b5312
kyle-rader <kyle.rader@ieee.org>
2015-02-17 01:29:07 -0800

    Add initial route for email subscription.

--- app/controllers/core.server.controller.js
+++ app/controllers/core.server.controller.js
@@ -3,9 +3,24 @@
 /**
  * Module dependencies.
  */
+var mailChimp = require('../../node_modules/mailchimp-api/mailchimp'),
+	validator = require('../../node_modules/email-validator'),
+	config = require('../../config/mc-config.json');
+
+var mc = new mailChimp.Mailchimp(config.apikey);
+
 exports.index = function(req, res) {
 	res.render('index', {
 		user: req.user || null,
 		request: req
+	});
+};
+
+exports.emailSubscribe = function(req, res) {
+	var validEmail = validator.validate(req.params.email);
+
+	res.send({
+		'email' : validEmail ? req.params.email : null,
+		'err' : !validEmail ? 'Oops! We need a valid email address :)' : ''
 	});
 };
--- app/routes/core.server.routes.js
+++ app/routes/core.server.routes.js
@@ -3,5 +3,9 @@
 module.exports = function(app) {
 	// Root routing
 	var core = require('../../app/controllers/core.server.controller');
+
+	app.route('/mail/subscribe/:email').post(core.emailSubscribe);
+
 	app.route('/').get(core.index);
+
 };
