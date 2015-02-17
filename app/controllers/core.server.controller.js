83cae8cd5af3c8212e585f38d6e740e2d79ea1c9
kyle-rader <kyle.rader@ieee.org>
2015-02-17 02:19:51 -0800

    Implement mail chimp subscribe web service.

--- app/controllers/core.server.controller.js
+++ app/controllers/core.server.controller.js
@@ -19,8 +19,15 @@ exports.index = function(req, res) {
 exports.emailSubscribe = function(req, res) {
 	var validEmail = validator.validate(req.params.email);
 
-	res.send({
-		'email' : validEmail ? req.params.email : null,
-		'err' : !validEmail ? 'Oops! We need a valid email address :)' : ''
-	});
+	if (validEmail) {
+		mc.lists.subscribe({id: req.params.listId, email:{email:req.params.email}},
+		function(data) {
+			res.send({email: req.params.eamil});
+		},
+		function(error) {
+			res.send({email: null, error: 'There was an error subscribing that email.\n' + error.error});
+		});
+	} else {
+		res.send({email: null, error: 'Invalid email address format'});
+	}
 };
--- app/routes/core.server.routes.js
+++ app/routes/core.server.routes.js
@@ -4,7 +4,7 @@ module.exports = function(app) {
 	// Root routing
 	var core = require('../../app/controllers/core.server.controller');
 
-	app.route('/mail/subscribe/:email').post(core.emailSubscribe);
+	app.route('/mail/subscribe/:listId/:email').post(core.emailSubscribe);
 
 	app.route('/').get(core.index);
 
