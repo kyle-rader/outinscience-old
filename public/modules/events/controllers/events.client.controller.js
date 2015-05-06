8743a92b2896d3505adaea6ffdd477057c9577c9
kyle-rader <kyle.rader@ieee.org>
2015-05-06 08:46:00 -0700

    Add Initial Events page listing.

--- public/modules/events/controllers/events.client.controller.js
+++ public/modules/events/controllers/events.client.controller.js
@@ -2,7 +2,27 @@
 
 angular.module('events').controller('EventsController', ['$scope',
 	function($scope) {
-		// Controller Logic
-		// ...
+
+		// TODO: Get events from a database or something.  Don't hard code.
+		$scope.events = [
+			{
+				title: 'WWU Open Source Day',
+				day: 'Saturday',
+				dayNum: 9,
+				month: 'May',
+				monthNum: 5,
+				description: 'Dive into the world of open source and see how you can contribute to projects and make a difference today!',
+				link: 'https://cse.wwu.edu/computer-science/event/open-source-day'
+			},
+			{
+				title: 'Whatcom Robotics Expo',
+				day: 'Saturday',
+				dayNum: 27,
+				month: 'June',
+				monthNum: 6,
+				description: 'Whatcom county is full of different K-12 robotics groups.  Experience them all under one roof and build your path in engineering from Kindergarten to College!',
+				link: 'https://www.facebook.com/events/1076239542392620/'
+			}
+		]
 	}
 ]);
