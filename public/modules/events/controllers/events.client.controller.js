9252dc547087872b217d27ba474fe443a4a26d0c
kyle-rader <kyle.rader@ieee.org>
2015-05-09 06:25:53 -0700

    Add table projects to events page for OSD.

--- public/modules/events/controllers/events.client.controller.js
+++ public/modules/events/controllers/events.client.controller.js
@@ -16,22 +16,27 @@ angular.module('events').controller('EventsController', ['$scope',
 				info:
 '<dl>\
 	<dt>Suggested Resources</dt>\
-	<dd>Github\'s Atom text editor. &#x21e8; <a target="_BLANK" href="https://atom.io/">https://atom.io/</a></dd>\
+	<dd>Github\'s Atom text editor. <br><a target="_BLANK" href="https://atom.io/">https://atom.io/</a></dd>\
 	<dt>Git Resources</dt>\
-	<dd>Try Git tutorial. <a target="_BLANK" href="https://try.github.io">https://try.github.io</a></dd>\
-	<dd>Git Bash. <a target="_BLANK" href="http://git-scm.com/downloads">http://git-scm.com/downloads</a></dd>\
-	<dd>Github for Mac. <a target="_BLANK" href="https://mac.github.com/">https://mac.github.com/</a></dd>\
-	<dd>Github for Windows. <a target="_BLANK" href="https://windows.github.com/">https://windows.github.com/</a></dd>\
-	<dt>Suggested Projects</dt>\
-	<dd>MediaWiki <a target="_blank" href="https://openhatch.org/projects/MediaWiki">https://openhatch.org/projects/MediaWiki</a> (lots of bite-size bugs, written in PHP)</dd>\
-	<dd>openemr <a target="_blank" href="https://openhatch.org/projects/openemr">https://openhatch.org/projects/openemr</a> (social interest, looks beginner friendly)</dd>\
-	<dd>OLPC <a target="_blank" href="https://openhatch.org/projects/OLPC">https://openhatch.org/projects/OLPC</a> (One Laptop per Child, social interest)</dd>\
-	<dd>gedit <a target="_blank" href="http://openhatch.org/projects/gedit">http://openhatch.org/projects/gedit</a> (bite-sized bugs, documentation)</dd>\
-	<dd>Mifos <a target="_blank" href="http://openhatch.org/projects/Mifos">http://openhatch.org/projects/Mifos</a> (social interest, written in Java)</dd>\
-	<dd>Dreamwidth <a target="_blank" href="https://openhatch.org/projects/Dreamwidth">https://openhatch.org/projects/Dreamwidth</a> (lots of bite-sized bugs)</dd>\
-	<dd>openspending <a target="_blank" href="https://openhatch.org/projects/openspending">https://openhatch.org/projects/openspending</a> (social/political interest, written in Python)</dd>\
-	<dd>sympy <a target="_blank" href="https://openhatch.org/projects/sympy">https://openhatch.org/projects/sympy</a> (for math lovers, lots of bite-sized bugs, written in Python)</dd>\
-	<dd>Firefox <a target="_blank" href="https://openhatch.org/projects/Firefox">https://openhatch.org/projects/Firefox</a> (lots of bite-sized bugs)</dd>\
+	<dd>Try Git tutorial. <br><a target="_BLANK" href="https://try.github.io">https://try.github.io</a></dd>\
+	<dd>Git Bash. <br><a target="_BLANK" href="http://git-scm.com/downloads">http://git-scm.com/downloads</a></dd>\
+	<dd>Github for Mac. <br><a target="_BLANK" href="https://mac.github.com/">https://mac.github.com/</a></dd>\
+	<dd>Github for Windows. <br><a target="_BLANK" href="https://windows.github.com/">https://windows.github.com/</a></dd>\
+	<dt>Dedicated Table Projects</dt>\
+	<dd><strong>Drupal</strong> (Jennifer Dixey) <br><a target="_blank" href="https://www.drupal.org/">https://www.drupal.org/</a></dd>\
+	<dd><strong>KeyMail (NachoBits)</strong> (Aaron Griffin) <br><a target="_blank" href="https://github.com/NachoBits/keymail">https://github.com/NachoBits/keymail</a></dd>\
+	<dd><strong>Language of Languages</strong> (Jamie & Andi Douglass) <br><a target="_blank" href="https://github.com/jamiedouglass/LanguageOfLanguages">https://github.com/jamiedouglass/LanguageOfLanguages</a></dd>\
+	<dd><strong>Able Player</strong> (Terry Thompson) <br><a target="_blank" href="https://github.com/ableplayer/ableplayer">https://github.com/ableplayer/ableplayer</a></dd>\
+	<dt>Other Suggested Projects</dt>\
+	<dd><strong>MediaWiki</strong> (lots of bite-size bugs, written in PHP) <br><a target="_blank" href="https://openhatch.org/projects/MediaWiki">https://openhatch.org/projects/MediaWiki</a> </dd>\
+	<dd><strong>openemr</strong> (social interest, looks beginner friendly) <br><a target="_blank" href="https://openhatch.org/projects/openemr">https://openhatch.org/projects/openemr</a> </dd>\
+	<dd><strong>OLPC</strong> (One Laptop per Child, social interest) <br><a target="_blank" href="https://openhatch.org/projects/OLPC">https://openhatch.org/projects/OLPC</a> </dd>\
+	<dd><strong>gedit</strong> (bite-sized bugs, documentation) <br><a target="_blank" href="http://openhatch.org/projects/gedit">http://openhatch.org/projects/gedit</a> </dd>\
+	<dd><strong>Mifos</strong> (social interest, written in Java) <br><a target="_blank" href="http://openhatch.org/projects/Mifos">http://openhatch.org/projects/Mifos</a> </dd>\
+	<dd><strong>Dreamwidth</strong> (lots of bite-sized bugs) <br><a target="_blank" href="https://openhatch.org/projects/Dreamwidth">https://openhatch.org/projects/Dreamwidth</a> </dd>\
+	<dd><strong>openspending</strong> (social/political interest, written in Python) <br><a target="_blank" href="https://openhatch.org/projects/openspending">https://openhatch.org/projects/openspending</a> </dd>\
+	<dd><strong>sympy</strong> (for math lovers, lots of bite-sized bugs, written in Python) <br><a target="_blank" href="https://openhatch.org/projects/sympy">https://openhatch.org/projects/sympy</a> </dd>\
+	<dd><strong>Firefox</strong> (lots of bite-sized bugs) <br><a target="_blank" href="https://openhatch.org/projects/Firefox">https://openhatch.org/projects/Firefox</a> </dd>\
 </dl>'
 			},
 			{
