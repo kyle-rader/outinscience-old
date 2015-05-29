caa4a8e6f987ca7dce7402688ac35b3b67d4399f
kyle-rader <kyle.rader@ieee.org>
2015-05-29 01:14:51 -0700

    Get color buttons working in header.  Add fluid container to header.

--- public/modules/core/config/core.client.config.js
+++ public/modules/core/config/core.client.config.js
@@ -4,7 +4,8 @@
 angular.module('core').run(['Menus',
 	function(Menus) {
 		// Set top bar menu items
-		Menus.addMenuItem('topbar', 'The Team', 'team', '', '/team', true);
+		Menus.addMenuItem('topbar', 'The Team', 'team', 'li-rb-purple', '/team', true);
+		Menus.addMenuItem('topbar', 'The Out list', 'out-list', 'li-rb-blue', '/the-out-list', true);
 
 	}
 ]);
---
+++ public/modules/core/controllers/team.client.controller.js
@@ -0,0 +1,8 @@
+'use strict';
+
+angular.module('core').controller('TeamController', ['$scope',
+	function($scope) {
+		// Team controller logic
+		// ...
+	}
+]);
