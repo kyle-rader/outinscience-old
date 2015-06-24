994904441b698b370a96753ea4e24034fe094521
kyle-rader <kyle.rader@ieee.org>
2015-06-24 03:52:56 -0700

    Make the homepage pretty.

--- public/modules/core/controllers/home.client.controller.js
+++ public/modules/core/controllers/home.client.controller.js
@@ -6,5 +6,23 @@ angular.module('core').controller('HomeController', ['$scope', 'Authentication',
 		// This provides Authentication context.
 		$scope.authentication = Authentication;
 
+		$scope.fields = {
+			'biology': 'Biology',
+			'astronomy': 'Astronomy',
+			'vehicleDesign': 'Vehicle Design',
+			'scienceEducation': 'Science Education',
+			'plasticsComposites': 'Plastics Composites',
+			'physics': 'Physics',
+			'neuroscience': 'Neuroscience',
+			'math': 'Mathematics',
+			'materialScience': 'Material Science',
+			'manufacturingEngineering': 'Manufacturing Engineering',
+			'industrialDesign': 'Industrial Design',
+			'environmentalScience': 'Environmental Science',
+			'electricalEngineering': 'Electrical Engineering',
+			'computerScience': 'Computer Science',
+			'chemistry': 'Chemistry',
+			'geology': 'Geology'
+		}
 	}
 ]);
