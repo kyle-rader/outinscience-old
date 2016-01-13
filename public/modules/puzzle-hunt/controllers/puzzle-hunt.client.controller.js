16b258f7bf6586de9b5e801ba5e64aec3788c345
KyleRader <kyle.rader@ieee.org>
2016-01-12 23:15:25 -0800

    Adding first content to puzzle hunt page and got centered title CSS worked out.

--- public/modules/puzzle-hunt/controllers/puzzle-hunt.client.controller.js
+++ public/modules/puzzle-hunt/controllers/puzzle-hunt.client.controller.js
@@ -1,30 +1,8 @@
 'use strict';
 
-angular.module('the-out-list').controller('TheOutListController', ['$scope', 'Authentication', '$http',
+angular.module('the-out-list').controller('PuzzleHuntController', ['$scope', 'Authentication', '$http',
 	function($scope, Authentication, $http) {
 		$scope.user = Authentication.user;
 
-		$http.get('/outlist')
-			.success(function(data, status, headers, config) {
-				$scope.outlist = data;
-			});
 	}
-]).directive('outlistEntry', function() {
-	return {
-		restrict: 'E',
-		scope: {
-			user:'=user'
-		},
-		replace: true,
-		templateUrl: '/modules/the-out-list/views/templates/outlist-entry.client.view.template.html'
-	};
-}).directive('primaryTags', function() {
-	return {
-		restrict: 'E',
-		scope: {
-			tags: '=tags'
-		},
-		replace: true,
-		templateUrl: '/modules/the-out-list/views/templates/tags.client.view.template.html'
-	};
-});
+]);
