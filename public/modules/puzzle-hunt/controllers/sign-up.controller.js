a3144d0f8930554b884ee1a7edfa72a2eba2805b
KyleRader <kyle.rader@ieee.org>
2016-01-17 17:15:06 -0800

    Adding initial puzzle hunt sign up page.

--- public/modules/puzzle-hunt/controllers/sign-up.controller.js
+++ public/modules/puzzle-hunt/controllers/sign-up.controller.js
@@ -1,8 +1,23 @@
 'use strict';
 
-angular.module('puzzle-hunt').controller('PuzzleHuntSignUpController', ['$scope', 'Authentication', '$http',
-	function($scope, Authentication, $http) {
+angular.module('puzzle-hunt').controller('PuzzleHuntSignUpController', ['$scope', 'Authentication', '$http', '$location',
+	function($scope, Authentication, $http, $location) {
 		$scope.user = Authentication.user;
 
+    // If user is signed in then redirect back home
+		if ($scope.authentication.user) $location.path('/#!/puzzle-hunt/dashboard');
+
+    $scope.signup = function() {
+			$http.post('/puzzlehunt/auth/signup', $scope.credentials).success(function(response) {
+				// If successful we assign the response to the global user model
+				delete $scope.error;
+				delete $scope.credentials;
+				$scope.userForm.$setPristine();
+				$scope.success = response.message;
+
+			}).error(function(response) {
+				$scope.error = response.message;
+			});
+		};
 	}
 ]);
